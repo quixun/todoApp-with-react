b18b4467f282da6e8bd64c080301e003e9ba8da2
Quixun <xuanphan742@gmail.com>
2024-08-26 10:47:10 +0700

    Create the Input Field and Add Button

--- src/ToDoApp.tsx
+++ src/ToDoApp.tsx
@@ -8,10 +8,32 @@ interface Task {
 
 function ToDoApp() {
   const [tasks, setTasks] = useState<Task[]>([]);
-  const [job, setJob] = useState('')
+  const [job, setJob] = useState("");
+
+  const handleAddTask = () => {
+    const newTask = {
+      id: Math.random(),
+      text: job,
+      completed: false,
+    };
+    setTasks([...tasks, newTask]);
+  };
   return (
-    <div className="flex items-center justify-center h-screen gap-3">
-      <h1>Hello world</h1>
+    <div className="flex items-center justify-center h-screen gap-3 flex-col">
+      <div className="flex items-center justify-center gap-4">
+        <input
+          className="bg-yellow-200 text-gray-900 focus:outline-lime-600"
+          type="text"
+          value={job}
+          onChange={(e) => setJob(e.target.value)}
+        />
+        <button
+          className="bg-yellow-200 text-gray-900 border-solid border w-20 border-[#333] rounded-md"
+          onClick={handleAddTask}
+        >
+          Add Task
+        </button>
+      </div>
     </div>
   );
 }
